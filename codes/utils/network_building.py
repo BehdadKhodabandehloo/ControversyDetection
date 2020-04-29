16a53cb955388a2d3de8cbe968839eaef29dc7e9
mehrdad <sayad.mehrdad@gmail.com>
2020-04-30 03:27:35 +0430

    fixing bug for dynamic graph

--- codes/utils/network_building.py
+++ codes/utils/network_building.py
@@ -1,9 +1,10 @@
 import networkx as nx
 from utils import *
 from data_loader import Dataloader
+import copy
 
 
-def static_retweet_graph(data):
+def static_retweet_graph(data, Graph=None):
     Original_Username = [] # username of who tweet
     Retweeting_Username = [] # user name of who retweet
     
@@ -19,13 +20,16 @@ def static_retweet_graph(data):
             
     # Remove Duplicate Elements
     edges_list = list(set(edges_to_add))
-        
-    Graph = nx.Graph()
+
+    if Graph is None:
+        Graph = nx.Graph()
     
     # Adding Weights to Graph
     for i in range(len(edges_list)):
-        Graph.add_edges_from([edges_list[i]], weight=edges_to_add.count(edges_list[i]))
-        
+        if Graph.has_edge(edges_list[i][0], edges_list[i][1]):
+            Graph[edges_list[i][0]][edges_list[i][1]]['weight'] += edges_to_add.count(edges_list[i])
+        else:
+            Graph.add_edges_from([edges_list[i]], weight=edges_to_add.count(edges_list[i]))
     return Graph
 
 
@@ -47,8 +51,14 @@ def dynamic_retweet_graph(data, discrete_bin=3600):
             times = key.split('_')
             if int(times[0]) <= item['created_at'] < int(times[1]):
                 dygraph[key].append(item)
+    previous_key = None
     for key, graph_list in dygraph.items():
-        dygraph[key] = static_retweet_graph(graph_list)
+        if previous_key is None:
+            dygraph[key] = static_retweet_graph(graph_list)
+        else:
+            temp_graph = copy.deepcopy(dygraph[previous_key])
+            dygraph[key] = static_retweet_graph(graph_list, temp_graph)
+        previous_key = key
     return dygraph
 
 
@@ -65,52 +75,51 @@ if __name__ == '__main__':
         count += 1
 
 # 1. graph (1430020362_1430023962) --> nodes = 11, edges = 9
-# 2. graph (1430023962_1430027562) --> nodes = 7, edges = 4
-# 3. graph (1430027562_1430031162) --> nodes = 105, edges = 100
-# 4. graph (1430031162_1430034762) --> nodes = 107, edges = 99
-# 5. graph (1430034762_1430038362) --> nodes = 36, edges = 29
-# 6. graph (1430038362_1430041962) --> nodes = 73, edges = 71
-# 7. graph (1430041962_1430045562) --> nodes = 48, edges = 39
-# 8. graph (1430045562_1430049162) --> nodes = 18, edges = 13
-# 9. graph (1430049162_1430052762) --> nodes = 18, edges = 13
-# 10. graph (1430052762_1430056362) --> nodes = 14, edges = 9
-# 11. graph (1430056362_1430059962) --> nodes = 30, edges = 23
-# 12. graph (1430059962_1430063562) --> nodes = 32, edges = 22
-# 13. graph (1430063562_1430067162) --> nodes = 64, edges = 54
-# 14. graph (1430067162_1430070762) --> nodes = 97, edges = 86
-# 15. graph (1430070762_1430074362) --> nodes = 76, edges = 62
-# 16. graph (1430074362_1430077962) --> nodes = 70, edges = 56
-# 17. graph (1430077962_1430081562) --> nodes = 72, edges = 60
-# 18. graph (1430081562_1430085162) --> nodes = 77, edges = 61
-# 19. graph (1430085162_1430088762) --> nodes = 76, edges = 56
-# 20. graph (1430088762_1430092362) --> nodes = 52, edges = 41
-# 21. graph (1430092362_1430095962) --> nodes = 62, edges = 51
-# 22. graph (1430095962_1430099562) --> nodes = 45, edges = 33
-# 23. graph (1430099562_1430103162) --> nodes = 68, edges = 48
-# 24. graph (1430103162_1430106762) --> nodes = 69, edges = 47
-# 25. graph (1430106762_1430110362) --> nodes = 72, edges = 56
-# 26. graph (1430110362_1430113962) --> nodes = 57, edges = 39
-# 27. graph (1430113962_1430117562) --> nodes = 55, edges = 33
-# 28. graph (1430117562_1430121162) --> nodes = 87, edges = 70
-# 29. graph (1430121162_1430124762) --> nodes = 48, edges = 33
-# 30. graph (1430124762_1430128362) --> nodes = 20, edges = 15
-# 31. graph (1430128362_1430131962) --> nodes = 20, edges = 11
-# 32. graph (1430131962_1430135562) --> nodes = 6, edges = 3
-# 33. graph (1430135562_1430139162) --> nodes = 4, edges = 2
-# 34. graph (1430139162_1430142762) --> nodes = 13, edges = 7
-# 35. graph (1430142762_1430146362) --> nodes = 16, edges = 9
-# 36. graph (1430146362_1430149962) --> nodes = 35, edges = 28
-# 37. graph (1430149962_1430153562) --> nodes = 49, edges = 43
-# 38. graph (1430153562_1430157162) --> nodes = 73, edges = 57
-# 39. graph (1430157162_1430160762) --> nodes = 55, edges = 39
-# 40. graph (1430160762_1430164362) --> nodes = 55, edges = 43
-# 41. graph (1430164362_1430167962) --> nodes = 80, edges = 63
-# 42. graph (1430167962_1430171562) --> nodes = 92, edges = 73
-# 43. graph (1430171562_1430175162) --> nodes = 56, edges = 39
-# 44. graph (1430175162_1430178762) --> nodes = 256, edges = 231
-# 45. graph (1430178762_1430182362) --> nodes = 1754, edges = 1797
-# 46. graph (1430182362_1430185962) --> nodes = 10633, edges = 13403
-# 47. graph (1430185962_1430189562) --> nodes = 16182, edges = 19452
-# 48. graph (1430189562_1430193162) --> nodes = 24434, edges = 30025
-# 49. graph (1430193162_1430196762) --> nodes = 1020, edges = 796
-
+# 2. graph (1430023962_1430027562) --> nodes = 15, edges = 13
+# 3. graph (1430027562_1430031162) --> nodes = 118, edges = 113
+# 4. graph (1430031162_1430034762) --> nodes = 204, edges = 207
+# 5. graph (1430034762_1430038362) --> nodes = 230, edges = 235
+# 6. graph (1430038362_1430041962) --> nodes = 297, edges = 306
+# 7. graph (1430041962_1430045562) --> nodes = 335, edges = 345
+# 8. graph (1430045562_1430049162) --> nodes = 349, edges = 358
+# 9. graph (1430049162_1430052762) --> nodes = 363, edges = 370
+# 10. graph (1430052762_1430056362) --> nodes = 372, edges = 379
+# 11. graph (1430056362_1430059962) --> nodes = 393, edges = 402
+# 12. graph (1430059962_1430063562) --> nodes = 414, edges = 424
+# 13. graph (1430063562_1430067162) --> nodes = 468, edges = 478
+# 14. graph (1430067162_1430070762) --> nodes = 554, edges = 564
+# 15. graph (1430070762_1430074362) --> nodes = 614, edges = 624
+# 16. graph (1430074362_1430077962) --> nodes = 665, edges = 677
+# 17. graph (1430077962_1430081562) --> nodes = 722, edges = 736
+# 18. graph (1430081562_1430085162) --> nodes = 780, edges = 797
+# 19. graph (1430085162_1430088762) --> nodes = 837, edges = 853
+# 20. graph (1430088762_1430092362) --> nodes = 872, edges = 894
+# 21. graph (1430092362_1430095962) --> nodes = 916, edges = 944
+# 22. graph (1430095962_1430099562) --> nodes = 948, edges = 975
+# 23. graph (1430099562_1430103162) --> nodes = 997, edges = 1023
+# 24. graph (1430103162_1430106762) --> nodes = 1044, edges = 1070
+# 25. graph (1430106762_1430110362) --> nodes = 1097, edges = 1126
+# 26. graph (1430110362_1430113962) --> nodes = 1137, edges = 1163
+# 27. graph (1430113962_1430117562) --> nodes = 1174, edges = 1195
+# 28. graph (1430117562_1430121162) --> nodes = 1250, edges = 1265
+# 29. graph (1430121162_1430124762) --> nodes = 1279, edges = 1298
+# 30. graph (1430124762_1430128362) --> nodes = 1292, edges = 1312
+# 31. graph (1430128362_1430131962) --> nodes = 1303, edges = 1323
+# 32. graph (1430131962_1430135562) --> nodes = 1305, edges = 1326
+# 33. graph (1430135562_1430139162) --> nodes = 1307, edges = 1328
+# 34. graph (1430139162_1430142762) --> nodes = 1312, edges = 1334
+# 35. graph (1430142762_1430146362) --> nodes = 1320, edges = 1343
+# 36. graph (1430146362_1430149962) --> nodes = 1345, edges = 1371
+# 37. graph (1430149962_1430153562) --> nodes = 1380, edges = 1409
+# 38. graph (1430153562_1430157162) --> nodes = 1433, edges = 1463
+# 39. graph (1430157162_1430160762) --> nodes = 1465, edges = 1500
+# 40. graph (1430160762_1430164362) --> nodes = 1502, edges = 1540
+# 41. graph (1430164362_1430167962) --> nodes = 1559, edges = 1598
+# 42. graph (1430167962_1430171562) --> nodes = 1615, edges = 1670
+# 43. graph (1430171562_1430175162) --> nodes = 1646, edges = 1708
+# 44. graph (1430175162_1430178762) --> nodes = 1850, edges = 1935
+# 45. graph (1430178762_1430182362) --> nodes = 3419, edges = 3708
+# 46. graph (1430182362_1430185962) --> nodes = 13296, edges = 17029
+# 47. graph (1430185962_1430189562) --> nodes = 27128, edges = 36162
+# 48. graph (1430189562_1430193162) --> nodes = 47389, edges = 65672
+# 49. graph (1430193162_1430196762) --> nodes = 47891, edges = 66431
