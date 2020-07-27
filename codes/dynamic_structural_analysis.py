d300d11faafcca4f9c4e8c4df0e89fda6d375d0a
BehdadKhodabandehloo <behdad.khodabandehloo@gmail.com>
2020-07-27 19:51:33 +0430

    "minor improvement"

--- codes/dynamic_structural_analysis.py
+++ codes/dynamic_structural_analysis.py
@@ -21,9 +21,13 @@ def all_snapshots(retweet_snapshots, mention_snapshots, reply_snapshots):
         snapshot_tuples.append((retweet_snapshots[key], mention_snapshots[key], reply_snapshots[key]))
     return snapshot_tuples
 
+snapshots = all_snapshots(retweet_snapshots, mention_snapshots, reply_snapshots)
 
-# obtain three graphs in each snapshot
-def three_graph(retweet_graph, mention_graph, reply_graph, graph_type = 'retweet'):
+# obtain three graphs in each snapshot in graph_type
+def three_graph(snapshot_tuple, graph_type):
+    retweet_graph = snapshot_tuple[0]
+    mention_graph = snapshot_tuple[1]
+    reply_graph = snapshot_tuple[2]
     partitions = nxmetis.partition(retweet_graph.to_undirected(), 2)
     left_side_nodes = partitions[1][0]
     right_side_nodes = partitions[1][1]
@@ -39,19 +43,22 @@ def three_graph(retweet_graph, mention_graph, reply_graph, graph_type = 'retweet
         main_graph = reply_graph
         left_side = reply_graph.subgraph(left_side_nodes)
         right_side = reply_graph.subgraph(right_side_nodes)
-    return (main_graph, left_side, right_side)
+    return tuple((main_graph, left_side, right_side))
+
+
+#retweettt = three_graph(snapshots[0], 'retweet') # return retweet and left and right sides of retweet graph in snapshot 1
 
 # obtain clustering coeff
-for i in range()
-graphs = three_graph(retweet_graph, mention_graph, reply_graph, graph_type = 'retweet')
+def clustering_coeff(snapshots, graph_type):
+    cc = []
+    for item in snapshots:
+        (main_graph,left_side, right_side) = three_graph(item, graph_type)
+        cc.append((nx.average_clustering(main_graph),
+                   nx.average_clustering(left_side),
+                   nx.average_clustering(right_side)))
+    return cc
 
-cc = []
-for item in graphs:
-    cc.append(nx.average_clustering(item))
+mention_clustering_coeff = clustering_coeff(snapshots, graph_type = 'mention')
 
-
-
-
-
-
+# degree distributions
 
