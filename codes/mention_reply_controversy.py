b75c4087ec3df42a282cfebcf570b02393be8958
mehrdad <sayad.mehrdad@gmail.com>
2020-08-04 13:31:57 +0430

    add directed option to network building

--- codes/mention_reply_controversy.py
+++ codes/mention_reply_controversy.py
@@ -5,26 +5,26 @@ from utils.network_building import *
 import nxmetis
 
 if __name__ == '__main__':
-    print('load dataset')
-    for file in ['germanwings_data', 'indiana_data', 'indiasdaughter_data', 'leadersdebate_data', 'mothersday_data',
-                 'baltimore_data', 'beefban_data']:
-        dataloader = Dataloader('/root/tweets_dataset')
-        dataset = dataloader.load_files(file)
+        for file in ['germanwings_data', 'indiana_data', 'indiasdaughter_data', 'leadersdebate_data', 'mothersday_data',
+                     'baltimore_data', 'beefban_data']:
+            print('load dataset : %s' % file)
+            dataloader = Dataloader('/root/tweets_dataset')
+            dataset = dataloader.load_files(file)
 
-        # mention graph analysis
-        mention_graph = static_mention_graph(dataset)
-        partitions = nxmetis.partition(mention_graph, 2)
-        bt_con = betweenness_centrality_controversy(mention_graph, partitions)
-        RWW_con = random_walk_conteroversy(mention_graph, partitions, 1000)
-        print("mention graph controversy measures")
-        print("Betweennes Measure = %s" % bt_con)
-        print("RWW Measure = %s" % RWW_con)
+            # mention graph analysis
+            mention_graph = static_mention_graph(dataset)
+            partitions = nxmetis.partition(mention_graph, 2)
+            bt_con = betweenness_centrality_controversy(mention_graph, partitions)
+            # RWW_con = random_walk_conteroversy(mention_graph, partitions, 1000)
+            print("mention graph controversy measures")
+            print("Betweennes Measure = %s" % bt_con)
+            # print("RWW Measure = %s" % RWW_con)
 
-        # reply graph analysis
-        reply_graph = static_reply_graph(dataset)
-        partitions = nxmetis.partition(reply_graph, 2)
-        bt_con = betweenness_centrality_controversy(reply_graph, partitions)
-        RWW_con = random_walk_conteroversy(reply_graph, partitions, 1000)
-        print("reply graph controversy measures")
-        print("Betweennes Measure = %s" % bt_con)
-        print("RWW Measure = %s" % RWW_con)
+            # reply graph analysis
+            reply_graph = static_reply_graph(dataset)
+            partitions = nxmetis.partition(reply_graph, 2)
+            bt_con = betweenness_centrality_controversy(reply_graph, partitions)
+            # RWW_con = random_walk_conteroversy(reply_graph, partitions, 1000)
+            print("reply graph controversy measures")
+            print("Betweennes Measure = %s" % bt_con)
+            # print("RWW Measure = %s" % RWW_con)
